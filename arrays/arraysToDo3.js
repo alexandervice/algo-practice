949eaddeca43af49bdd9973813e3a3c87c3caf20
Alexander Vice <alexdvice@gmail.com>
2023-07-24 10:08:36 -0700

    Update arraysToDo3.js

--- arrays/arraysToDo3.js
+++ arrays/arraysToDo3.js
@@ -155,4 +155,32 @@ const nthLargest = (arr, n) => {
   return (min === -Infinity) ? null : min;
 };
 
+// console.log(nthLargest(arr5,3))
 
+//______________________________________________________________________________
+
+// Skyline Heights
+// ****** Directions ******
+// Lovely Burbank has a breathtaking view of the Los Angeles skyline. Let’s say you are given an array with heights of consecutive buildings, starting closest to you and extending away. Array [-1,7,3] would represent three buildings: first is actually out of view below street level, behind it is second at 7 stories high, third is 3 stories high (hidden behind the 7-story). You are situated at street level. Return array containing heights of buildings you can see, in order. Given [-1,1,1,7,3] return [1,7]. Given [0,4] return [4]. As always with challenges, do not use built-in array functions such as unshift().
+
+// thought process:
+// start by making a new array to return of the visible buildings
+// make the code work for most cases
+// finalize for situational cases of excess negetives, 1st building being negative etc
+
+
+const skylineHeights = (arr) => {
+  let visibleBuildings = [];
+  let lastBuilding = 0;
+  for(let i=0; i<arr.length; i++) {
+    if(arr[i] > 0 && arr[i] > lastBuilding) {
+      visibleBuildings.push(arr[i]);
+      lastBuilding = arr[i]
+    }
+  }
+  return visibleBuildings
+}
+
+const arr8 = [-1,1,1,7,3]
+
+console.log(skylineHeights(arr8))
